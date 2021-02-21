02648868167f996d166210f9ae7735e26b65fd11
Joshua Geyer <jsg277@nau.edu>
2021-02-20 19:45:59 -0700

    Update Message.java
    added constructors for the message classes, so that a string can be set when a Message object is created

--- Message.java
+++ Message.java
@@ -4,14 +4,44 @@ public class Message {
 	
 	// The message may be set using a scanner class in the NodeMain
 	String message;
+	
+	//simple identifier for checking what kind of message this is
+	//0 for join message, 1 for leave message, 2 for any other message
+	int messageIdentifier;
+	
+	//constructor to set message
+	public Message(String input){
+		message = input;
+		messageIdentifier = 2;
+	}
 }
 
 public class Message_Join extends Message {
 	// Specific message for joining the chat
-	String joinMessage = " joined the chat";
+	String joinMessage;
+	
+	//simple identifier for checking what kind of message this is
+	//0 for join message, 1 for leave message, 2 for any other message	
+	int messageIdentifier;
+	
+	//construtor to set variables
+	public Message_Join(){
+		joinMessage = " joined the chat";
+		messageIdentifier = 0;
+	}
 }
 
 public class Message_Leave extends Message {
 	// Specific message for leaving the chat
-	String leaveMessage = " has left the chat";
+	String leaveMessage;
+	
+	//simple identifier for checking what kind of message this is
+	//0 for join message, 1 for leave message, 2 for any other message	
+	int messageIdentifier;
+	
+	//construtor to set variables
+	public Message_Join(){
+		joinMessage = " has left the chat";
+		messageIdentifier = 1;
+	}
 }
